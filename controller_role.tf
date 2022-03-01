e776ad9a4abd2562729267b92fee0d60ba4899a9
michaelvaltix <83365005+michaelvaltix@users.noreply.github.com>
2022-02-28 23:23:05 -0800

    Updating Controller IAM role (#1)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-81-94.ec2.internal>

--- controller_role.tf
+++ controller_role.tf
@@ -44,7 +44,12 @@ resource "aws_iam_role_policy" "valtix_controller_policy" {
           "route53resolver:*",
           "servicequotas:GetServiceQuota",
           "s3:ListAllMyBuckets",
-          "s3:ListBucket"
+          "s3:ListBucket",
+          "events:PutRule",
+          "events:PutTargets",
+          "events:DeleteRule",
+          "events:RemoveTargets",
+          "events:ListTargetsByRule"
         ],
         Effect   = "Allow",
         Resource = "*"

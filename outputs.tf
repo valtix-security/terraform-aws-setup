9da8bb9951c765256c630ad34526c443493078d7
Kiran Madabhushi <kiran@valtix.com>
2022-03-22 15:06:12 -0700

    add console urls and the cloud account name as the outputs

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "valtix_inventory_role_arn" {
 output "valtix_firewall_role_name" {
   value = aws_iam_role.valtix_fw_role.name
 }
+
+output "cloud_account_name" {
+  value = var.valtix_aws_cloud_account_name
+}
+
+output "z-console-urls" {
+  value = {
+    controller-role = "https://console.aws.amazon.com/iamv2/home?#/roles/details/${aws_iam_role.valtix_controller_role.name}"
+    firewall-role   = "https://console.aws.amazon.com/iamv2/home?#/roles/details/${aws_iam_role.firewall_role.name}"
+    inventory-role  = "https://console.aws.amazon.com/iamv2/home?#/roles/details/${aws_iam_role.inventory_role.name}"
+  }
+}
